0ba33a696f38f2d934f5c62fc5111194f38c70f8
Ryan Gillett <rcgillet@svsu.edu>
2013-10-07 00:14:55 -0400

    Added shop

---
+++ js/coffee.js
@@ -0,0 +1,214 @@
+var Blends = [
+{
+	"name": "Breakfast Blend", 
+	"onePound": "14.00",
+	"halfPound": "7.00",
+	"description": "Light Roast"
+},
+{
+	"name": "Frankenmuth City Blend", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Light/Medium Roast"
+},	
+{
+	"name": "Harvest House Blend", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Medium Roast"
+},
+{
+	"name": "Bolivia (Fair Trade/Organic", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Dark Roast"
+},
+{
+	"name": "Ethiopian Yirgacheffe", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Dark/Medium Roast"
+},
+{
+	"name": "Guatamala (Fair Trade/Organic)", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Light Roast"
+},
+{
+	"name": "Kona Blend", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Light Roast"
+},
+{
+	"name": "Mexican (Organic)", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Dark Roast"
+},
+{
+	"name": "Papua New Guinea", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Dark Roast"
+},
+{
+	"name": "Peru (Fair Trade/Organic)", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Light Roast"
+},
+{
+	"name": "Sumatra", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Dark Roast"
+},
+{
+	"name": "Yemen Mocha", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Light Roast"
+},
+{
+	"name": "Espresso", 
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": "Dark Roast"
+}];
+
+var decafBlends = [
+{
+	"name": "Decaf Colombian",
+	"onePound": "15.00",
+	"halfPound": "7.50"
+},
+{
+	"name": "Decaf House",
+	"onePound": "15.00",
+	"halfPound": "7.50"
+},
+{
+	"name": "Decaf Mexican",
+	"onePound": "16.00",
+	"halfPound": "8.00"
+},
+{
+	"name": "Decaf Espresso",
+	"onePound": "16.00",
+	"halfPound": "8.00"
+}];
+
+var Flavored = [
+{
+	"name": "Blueberry Cream", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "Caramel Nut", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "Frankenmuth Fudge", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "French Vanilla", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "Hazelnut", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "Traverse City Cherry", 
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": ""
+},
+{
+	"name": "Pumpkin Spice",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+},
+{
+	"name": "Michigan Maple",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+},
+{
+	"name": "Souther Pecan",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+}];
+
+var decafFlavored = [
+{
+	"name": "Decaf Blueberry Cream",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf Caramel Nut",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf Fudge",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf French Vanilla",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf Hazelnut",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf Cherry",
+	"onePound": "16.00",
+	"halfPound": "8.00",
+	"description": ""
+},
+{
+	"name": "Decaf Pumpkin Spice",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+},
+{
+	"name": "Decaf Michigan Maple",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+},
+{
+	"name": "Decaf Southern Pecan",
+	"onePound": "15.00",
+	"halfPound": "7.50",
+	"description": "Seasonal"
+},
+];
